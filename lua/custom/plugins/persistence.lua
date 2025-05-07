29d288345074e3ac0e1a4e528db5141de3270006
jcdiv47 <jackcai47@hotmail.com>
2025-05-07 17:16:32 +0800

    Update lazy-lock.json

--- lua/custom/plugins/persistence.lua
+++ lua/custom/plugins/persistence.lua
@@ -1,13 +1,20 @@
 return {
-  -- 'folke/persistence.nvim',
-  -- event = 'BufReadPre', -- this will only start session saving when an actual file was opened
-  -- opts = {
-  --   -- restore the session for the current directory
-  --   vim.api.nvim_set_keymap('n', '<leader>qs', [[<cmd>lua require("persistence").load()<cr>]], {}),
-  --   -- restore the last session
-  --   vim.api.nvim_set_keymap('n', '<leader>ql', [[<cmd>lua require("persistence").load({ last = true })<cr>]], {}),
-  --
-  --   -- stop Persistence => session won't be saved on exit
-  --   vim.api.nvim_set_keymap('n', '<leader>qd', [[<cmd>lua require("persistence").stop()<cr>]], {}),
-  -- },
+  'folke/persistence.nvim',
+  event = 'BufReadPre', -- this will only start session saving when an actual file was opened
+  config = function()
+    require('persistence').setup {
+      vim.keymap.set('n', '<leader>qs', function()
+        require('persistence').load()
+      end, { desc = 'Restore the session for the current directory' }), -- Restore the session for the current directory
+      vim.keymap.set('n', '<leader>ql', function()
+        require('persistence').load { last = true }
+      end, { desc = 'Restore the last session' }), -- Restore the last session
+      vim.keymap.set('n', '<leader>qS', function()
+        require('persistence').select()
+      end, { desc = 'Select a session to load' }), -- Select a session to load
+      vim.keymap.set('n', '<leader>qd', function()
+        require('persistence').stop()
+      end, { desc = 'Stop Persistence' }), -- Stop Persistence => session won't be saved on exit
+    }
+  end,
 }
